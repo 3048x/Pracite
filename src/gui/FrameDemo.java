d3ff6187e302daf7a335d1113ada115168bb7189
3048x <3048333172@qq.com>
2021-05-28 19:10:56 +0800

    Merge remote-tracking branch 'origin/master'

--- src/gui/FrameDemo.java
+++ src/gui/FrameDemo.java
@@ -1,9 +1,17 @@
 package gui;
-    public class FrameDemo {
-        public static void main(String[] args){
-            MyFrameDate frame2=new MyFrameDate("第二个窗口");
-            frame2.showTime();
-        }
-    }
 
+
+
+import gui.sin.MyFrameSin;
+
+import javax.swing.*;
+
+public class FrameDemo {
+    public static void main(String[] args) {
+
+        //MyFrameDate frame2=new MyFrameDate("第一个窗口");
+        //frame2.showTime();
+        MyFrameSin frameSin = new MyFrameSin("正弦曲线");
+    }
+}
 
